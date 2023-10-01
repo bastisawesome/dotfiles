966e7be43856cf020516eec6e85f9e86c01dd3d4
Giles <bastisawesomeltd@gmail.com>
2023-10-01 15:12:47 -0400

    Add pretty org bullets.

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -42,6 +42,7 @@
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
 
+(require 'org-bullets)(add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
 ;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
